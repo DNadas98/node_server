01e53fc6866d217db3a6b601ddeaeaec1d1ea5bc
Daniel Nadas <125133206+DNadas98@users.noreply.github.com>
2023-05-05 13:25:44 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -86,9 +86,8 @@ const server = http.createServer((req, res) => {
   }
   //Comment next line out to remove query filter
   filteredUrl = req.url.replace(`?${queryString}`, '');
-  
-  
-  //Remove request body and clear out cookies - we don't need those to serve static files
+    
+  //Remove request body and clear out some headers - not needed to serve static files
   req.body = {};
   delete req.headers.cookie;
   delete req.headers.referer;
